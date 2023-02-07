484087e9d796895d06c84762e3571fafbb68b281
Dayana <dpedrosoa@gmail.com>
2023-02-07 12:05:10 -0500

    Add Models

---
+++ GamesAPI/Models/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace GamesAPI.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<TeamPlayer> TeamPlayers { get; set; }
+    }
+}
---
+++ GamesAPI/Models/Team.cs
@@ -0,0 +1,10 @@
+﻿namespace GamesAPI.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<GameTeam> GameTeams { get; set; }
+        public ICollection<TeamPlayer> TeamPlayers { get; set; }
+    }
+}
